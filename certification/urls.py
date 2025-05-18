f95c4b4960cdcd5c50b6d74d124ff8bd4cf6c418
Steve Yonkeu <yokwejuste@yahoo.com>
2025-05-18 15:21:45 +0100

    refactor: organize static file handling in urls.py

--- certification/urls.py
+++ certification/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from .views import IndexView, AddCertificationView, AddRelatedView, CertificationDetailView
@@ -10,5 +8,3 @@ urlpatterns = [
     path("add-related/<str:model_name>/", AddRelatedView.as_view(), name="add_related"),
     path("certification/<int:pk>/", CertificationDetailView.as_view(), name="certification_detail"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
